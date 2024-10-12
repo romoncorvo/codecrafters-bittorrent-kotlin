b797d2aef9158f878b7cc4e6d5b3d625c61b0e04
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2024-10-12 19:47:29 +0200

    Simplify code so that I can actually understand it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,9 +11,7 @@ fun main(args: Array<String>) {
         "decode" -> {
             // Uncomment this block to pass the first stage
             val bencodedValue = args[1]
-            val bencode = Bencode()
-            val type = bencode.type(bencodedValue.toByteArray())
-            val decoded = bencode.decode(bencodedValue.toByteArray(), type)
+            val decoded = decode(bencodedValue)
             println(gson.toJson(decoded))
             return
         }
@@ -21,3 +19,68 @@ fun main(args: Array<String>) {
         else -> println("Unknown command $command")
     }
 }
+
+fun decode(input: String): Any {
+    return when {
+        Character.isDigit(input[0]) -> {
+            decodeString(input)
+        }
+
+        input[0] == 'i' -> {
+            decodeNumber(input)
+        }
+
+        input[0] == 'l' -> {
+            decodeList(MutableString(input))
+        }
+
+        else -> throw RuntimeException("Exception thrown during decoding")
+    }
+}
+
+private fun decodeNumber(input: String): Long {
+    val length = input.indexOfFirst { it == 'e' }
+    return input.substring(1, length).toLong()
+}
+
+private fun decodeString(input: String): String {
+    val firstColonIndex = input.indexOfFirst { it == ':' }
+    val length = Integer.parseInt(input.substring(0, firstColonIndex))
+    return input.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
+}
+
+private fun decodeList(input: MutableString): List<Any> {
+    input.value = input.value.substring(1)
+    val result: MutableList<Any> = ArrayList()
+
+    while (input.value.isNotEmpty() && input.value[0] != 'e') {
+        val element = decode(input.value)
+        result.add(element)
+        val encodedEl = encode(element)
+        input.value = input.value.substring(encodedEl.length)
+    }
+
+    return result
+}
+
+private fun encode(input: Any?): String {
+    return when (input) {
+        is Long -> {
+            "i${input}e"
+        }
+
+        is String -> {
+            "${input.length}:${input}"
+        }
+
+        is List<*> -> {
+            "l${input.joinToString { x -> encode(x) }}e"
+        }
+
+        else -> throw RuntimeException("Exception thrown during decoding")
+    }
+}
+
+data class MutableString(var value: String)
+
+
